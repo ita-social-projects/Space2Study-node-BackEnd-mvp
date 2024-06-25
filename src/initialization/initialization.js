1cdff979d01b7ef0bdf4b3bebabab028b318a5b2
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2024-06-25 17:45:25 +0300

    Changed access to backend (#133)
    * changed access
    
    * fixed

--- src/initialization/initialization.js
+++ src/initialization/initialization.js
@@ -15,7 +15,7 @@ const initialization = (app) => {
   app.use(cookieParser())
   app.use(
     cors({
-      origin: CLIENT_URL,
+      origin: process.env.NODE_ENV === 'development' ? true : CLIENT_URL,
       credentials: true,
       methods: 'GET, POST, PATCH, DELETE',
       allowedHeaders: 'Content-Type, Authorization'
